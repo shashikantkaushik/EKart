3835e4c85d653ac45f2ab115356694a0d734e415
Shashi Kant <shashikantkaushik4@gmail.com>
2023-10-29 22:58:56 +0530

    hf

--- server/index.js
+++ server/index.js
@@ -1,22 +1,35 @@
 console.log('hello world');
 const express=require('express');
-//iport
+const mongoose=require('mongoose');
+//import
 const authRouter=require("./routes/auth");
 const PORT=3000;
 
+const DB='mongodb+srv://shashikantkaushik:shashikant@cluster0.l7o4rfs.mongodb.net/?retryWrites=true&w=majority';
 const app=express();
 app.use(authRouter);
 
+mongoose.connect(DB).then(()=>{
+console.log('Connection is Successful to MongoDB');
+})
+.catch((e)=>{
+console.log(e);
+});
+
+
+
 //initialize
 
-app.get('/hello',(req,res)=>{
-res.send('hello World');
-});
 
-app.get('/',(req,res)=>{
-res.json({name:"shashi"});
-
-});
+//get request
+//app.get('/hello',(req,res)=>{
+//res.send('hello World');
+//});
+//
+//app.get('/',(req,res)=>{
+//res.json({name:"shashi"});
+//
+//});
 
 app.listen(PORT,"0.0.0.0",()=>{
 console.log(`connnect ed at port ${PORT}`);
